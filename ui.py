eefcd0292c1547a57aed75abd430d1d340125966
Aza090-lab <arielseveroax@gmail.com>
2025-11-06 14:58:47 +0000

    ui

--- ui.py
+++ ui.py
@@ -1,1 +1,7 @@
-
+def main_ui():
+    print("\n=== Gerenciador De Projetos ===\n[1] Usuários\n[2] Projetos\n[3] Tarefas")
+    m = input("\n...")
+    if m not in "1234567890":
+        print("\nAÇÃO INVALIDA!!!!")
+        main_ui()
+main_ui()
