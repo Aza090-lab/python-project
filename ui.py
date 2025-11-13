1e6f0063cbab4667b7e5bb3a7b7ae61b46e35943
Aza090-lab <arielseveroax@gmail.com>
2025-11-13 14:46:14 +0000

    ui usuario

--- ui.py
+++ ui.py
@@ -7,12 +7,13 @@ def main_ui():
         print("Ação invalida! Tente novamente...")
         main_ui()
     elif m == "1":
-        print("\nUsuários...")
-        in_usuario = models.in_usuario()
+        #print("\nUsuários...")
+        usuario_ui()
+        #in_usuario = models.in_usuario()
     elif m == "2":
         print("\nProjetos...")
     else:
         print("\nTarefas...")
 
 def usuario_ui():
-    
+    print("====== Usuários =====\n[1] Cadastrar\n[2] listar\n[3] Buscar\n[4] Atualizar\n[5] Remover ")
