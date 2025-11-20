9d68c1f716ae9dcc94114b1745430762bb4d3e11
Aza090-lab <arielseveroax@gmail.com>
2025-11-20 20:42:23 +0000

    Corrige referências de função e adiciona a função de atualização de usuário

--- codes/func.py
+++ codes/func.py
@@ -6,8 +6,11 @@ incluse evitem criar funções repetidas...
 
 #=====imports=====
 import ui
+
 #=====Variaveis=====
 usuarios_list = []
+what = "" #O tipo (nome ou e-mail) doq vai buscar
+obj = "" # A coisa que vai buscar
 
 def add_user():
     usuario = {'NOME': input("NOME..."), 'E-MAIL': input("E-MAIL..."), 'PERFIL': input("PERFIL...")}
@@ -23,7 +26,7 @@ def list_users():
 def search_user():
     if len(usuarios_list) == 0:
                 print("Nenhum usuário cadastrado!")
-                main_ui()
+                ui.main_ui()
 
     print("Buscar por...\n[1] Nome\n[2] E-mail")
     what = input("...")
@@ -43,9 +46,24 @@ def search_user():
         else:
             obj = input("Digite o e-mail...")
             for i in usuarios_list:
-                if i['NOME'] == obj:
+                if i['E-MAIL'] == obj:
                     print(i)
                     break
                 else:
                     print("Usuário não encontrado!")
         
+def update_user():
+     if len(usuarios_list)==0:
+          print("Nenhum usuário cadastrado!")
+          ui.main_ui()
+     print("Localizar usuário por...\n[1] Nome\n[2] E-mail")
+     what = input("...")
+     if what not in "12":
+          print("Opção invalida! Tente novamente...")
+          ui.main_ui()
+     else:
+          if what == "1":
+               obj = input("Digite o nome...")
+               for i in range(len(usuarios_list)):
+                    if usuarios_list[i['NOME']] == obj:
+                         print(usuarios_list[i['NOME']])
